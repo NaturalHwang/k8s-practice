be03ee5dfb4afa9420ea24645a0cf8f52e5df455
jasonhwang <junghahwang0601@gmail.com>
2024-08-30 15:32:01 +0900

    fix

---
+++ orderSystem/src/main/java/beyond/orderSystem/common/configs/CorsConfig.java
@@ -0,0 +1,19 @@
+package beyond.orderSystem.common.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("/**") // ex. localhost:8081/---/--- 모두 허용
+                .allowedOrigins("http://localhost:3000") // 허용 url 명시
+//                .allowedOrigins("https://www.jungha.store")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 보안처리 허용 여부
+    }
+}
